fb429641033f2ee15d09ac6b2080a5aa7ca75dd5
reynaldodendy <reynaldodendy01@student.ciputra.ac.id>
2026-09-19 16:44:35 +0700

    Update Enemy.kt

--- soal2/src/main/java/com/example/soal2/Enemy.kt
+++ soal2/src/main/java/com/example/soal2/Enemy.kt
@@ -6,10 +6,22 @@ class Enemy(
     private var type: String
 )
 {
-    fun setHp(value: Int) { HP = value }
-    fun getType(): String = type
-    fun setType(value: String) { type = value }
-    fun getName(): String = name
-    fun setName(value: String) { name = value }
-    fun getHp(): Int = HP
+    fun setHp(value: Int) {
+        HP = value
+    }
+    fun getType(): String {
+        return type
+    }
+    fun setType(value: String) {
+        type = value
+    }
+    fun getName(): String {
+        return name
+    }
+    fun setName(value: String) {
+        name = value
+    }
+    fun getHp(): Int {
+        return HP
+    }
 }
